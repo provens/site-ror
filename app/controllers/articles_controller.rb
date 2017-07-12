efcbc2c82904f69baba2d00e33e52e7cd5896517
Damian <sesepro@gmail.com>
2017-07-12 13:22:13 +0200

    changed img links to fit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-	http_basic_authenticate_with name: "daa", password: "daa", except: [:index, :show]
+	#http_basic_authenticate_with name: "daa", password: "daa", except: [:index, :show]
 
 	def index
 		@articles = Article.all
