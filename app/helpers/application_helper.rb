6e483047e00937583b07990ac58fdea174550363
Damian <sesepro@gmail.com>
2017-07-14 02:13:24 +0200

    added responsive title support for guest book

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
 	def link_to_image(image_path, target_link,options={})
 		link_to(image_tag(image_path, :border => "0"), target_link, options)
 	end
+	def title(page_title)
+		content_for(:title) { page_title }
+	end
 end
